e9aede092deb6b59ea85b3dfea4b20851607364d
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-12-03 18:48:00 -0300

    Update lista_noticias.php

--- lista_noticias.php
+++ lista_noticias.php
@@ -75,7 +75,10 @@ include "conn.php";
                         <img class='card-img-top' data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail' alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;' src='img/noticias/" . $row['imagen'] . "' data-holder-rendered='true'>
                         <div class='card-body'>
                         <p class='card-text'><b>" . $row['titulo'] . "</b></p>
-                        <div class='d-flex justify-content-between align-items-center'>";
+                        <div class='d-flex justify-content-between align-items-center'>
+                        <div class='btn-group'>
+                            <a href='ver_noticia.php?id=" . $row['id'] . "'target='_self'><button type='button' class='btn btn-sm btn-outline-secondary'>Ver noticia</button></a>
+                            </div>";
                         if (isset($_COOKIE["ADMIN"]) == '1'){
                             echo"
                             <div class='btn-group'>
