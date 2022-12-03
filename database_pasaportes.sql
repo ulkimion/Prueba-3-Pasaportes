c55694cdbca70a0da99f87c1c6177a46b16efe10
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-12-03 18:43:05 -0300

    noticias

--- database_pasaportes.sql
+++ database_pasaportes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2022 at 09:32 PM
+-- Generation Time: Dec 03, 2022 at 10:42 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `database_pasaportes`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `id` int(9) NOT NULL,
+  `titulo` varchar(30) NOT NULL,
+  `fecha` date NOT NULL,
+  `cuerpo` text NOT NULL,
+  `visitas` int(8) NOT NULL,
+  `imagen` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `noticias`
+--
+
+INSERT INTO `noticias` (`id`, `titulo`, `fecha`, `cuerpo`, `visitas`, `imagen`) VALUES
+(1, 'titulo', '2022-12-08', 'cuerpo', 1, 'noticia.png');
 
 -- --------------------------------------------------------
 
@@ -48,17 +70,24 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `nacionalidad`, `genero`, `ciudad_residencia`, `foto`, `fecha_nacimiento`, `rut`, `activo`, `administrador`, `contrasena`, `correo`) VALUES
-(1, 'w', 'w', 'Chilena', 'Femenino', 'weq', 'img/23-11-202218-01-23_6607w.png', '2022-11-02', 'wer', 1, 1, '', ''),
-(4, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-16-14_4682wqe.png', '2022-11-07', 'qwe', 1, 0, '', ''),
-(5, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-16-16_5167wqe.png', '2022-11-07', 'qwe', 1, 0, '', ''),
-(7, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-06_9641wqe.png', '2022-11-07', 'qwe', 0, 0, '', ''),
-(8, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-08_9573wqe.png', '2022-11-07', 'qwe', 0, 0, '', ''),
-(9, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-09_6303wqe.png', '2022-11-07', 'qwe', 0, 1, '', ''),
-(10, 'rew', 'wer', 'Chilena', 'Femenino', 'twewet', 'img/23-11-202221-32-12_6714rew.png', '2022-11-01', 'ewr', 0, 0, 'wre', 'wer');
+(1, 'w', 'w', 'Chilena', 'Femenino', 'weq', 'img/23-11-202218-01-23_6607w.png', '2022-11-02', 'wer', 1, 1, '1', '1'),
+(4, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-16-14_4682wqe.png', '2022-11-07', 'qwe', 1, 0, '2', '2'),
+(5, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-16-16_5167wqe.png', '2022-11-07', 'qwe', 1, 0, '3', '3'),
+(7, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-06_9641wqe.png', '2022-11-07', 'qwe', 0, 0, '4', '4'),
+(8, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-08_9573wqe.png', '2022-11-07', 'qwe', 0, 0, '6', '6'),
+(9, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-09_6303wqe.png', '2022-11-07', 'qwe', 0, 1, '7', '7'),
+(10, 'rew', 'wer', 'Chilena', 'Femenino', 'twewet', 'img/23-11-202221-32-12_6714rew.png', '2022-11-01', 'ewr', 0, 0, 'contrasena', 'correo'),
+(11, 'wqe', 'qwe', 'Chilena', 'Masculino', 'qrt', 'img/23-11-202219-17-08_9573wqe.png', '2022-11-07', 'qwe', 0, 0, '5', '5');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `usuarios`
@@ -71,10 +100,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT for table `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- lista_noticias.php
+++ lista_noticias.php
@@ -1,4 +1,6 @@
-
+<?php
+include "conn.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,29 +11,7 @@
     <title>Inicio</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-<style>
-  .div1 {
-  border: 5px  rgb(255, 255, 255);
-  background-color: rgb(197, 215, 222);
-  height: 400px;
-  width: 18%;
-  margin-top: 5px; 
-  margin-right: 17px; 
-  text-align: center;
-  float: right;
-  }
-
-  .div2{
-  border: 5px  rgb(255, 255, 255);
-  background-color: rgb(45, 123, 154);
-  height: 200px;
-  width: 18%;
-  margin-top: 50px; 
-  margin-left: 17px; 
-  text-align: center;
-  float: left;
-  }
-
+    <style>
   .usuario{
   background-color: rgb(45, 123, 154);
   height: 30px;
@@ -41,10 +21,9 @@
 
 </style>
 </head>
+
 <body>
-
-
-    <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark"">
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark"">
         <div class="container-fluid">
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -52,9 +31,9 @@
           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
               <a class="nav-link active" aria-current="page" href="index.html">Inicio</a>
-              <a class="nav-link" href="registro.php">Registrase</a>
+              <a class="nav-link" href="registro.php">Ingresar Persona</a>
               <a class="nav-link" href="listausuarios.php">Lista Personas</a>
-              <a class="nav-link" href="ingresar.php">Ingresar</a>
+              <a class="nav-link" href="ingresar.php">Registro</a>
               <a class="nav-link" href="lista_noticias.php">Noticias</a>
               <a class="nav-link" href="estadisticas.php">Estadisticas</a>
               <?php      
@@ -76,36 +55,46 @@
       else { echo "Aun no a ingresado";}
          ?>
         </div>
-      <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="1s">
-            <img src="carrusel/img1.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item" data-bs-interval="2000">
-            <img src="carrusel/img2.jpg" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="carrusel/img3.jpg" class="d-block w-100" alt="...">
-          </div>
+
+    </nav>
+
+    <br>
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <?php
+                $sql = "SELECT * from noticias ORDER BY id DESC";
+                $resultado = $conexion->query($sql);
+                if ($resultado->num_rows > 0) {
+                    while ($row = $resultado->fetch_assoc()) {
+
+                        echo "
+                    <div class='col-md-4'>
+                    <div class='card mb-4 box-shadow'>
+                        <img class='card-img-top' data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail' alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;' src='img/noticias/" . $row['imagen'] . "' data-holder-rendered='true'>
+                        <div class='card-body'>
+                        <p class='card-text'><b>" . $row['titulo'] . "</b></p>
+                        <div class='d-flex justify-content-between align-items-center'>";
+                        if (isset($_COOKIE["ADMIN"]) == '1'){
+                            echo"
+                            <div class='btn-group'>
+                            <a href='noticiasmod.php?id=" . $row['id'] . "'target='_self'><button type='button' class='btn btn-sm btn-outline-secondary'>Modificar</button></a>
+                            </div>";
+                        }
+                           echo" <small class='text-muted'>" . $row['fecha'] . "</small>
+                        </div>
+                        </div>
+                    </div>
+                    </div>";
+                    }
+                }
+                ?>
+            </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
+    </div>
 
-      <div class="div2">ayuda</div>
-      <div class="div1">ayuda</div>
-      <div class="div1">ayuda</div>
-      <div class="div1">ayuda</div>
-
-
-
-      <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
+    <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
           Tecnología Multimedia - CIF 6558 - 06/11/2022  <br>
             Benjamin Gonzalez Fredes <br>
@@ -115,4 +104,5 @@
       </footer>
 
 </body>
+
 </html>
