35089ee8b29f00dff47837892f676e9035ce5e9f
ulkimion <101999652+ulkimion@users.noreply.github.com>
2022-12-21 17:52:50 -0300

    Estadisticas

---
+++ estadisticas.php
@@ -0,0 +1,127 @@
+<?php
+include "conn.php";
+session_start();
+
+$Chilena = mysqli_query($conexion, "select count(*) as cantidad from usuarios where nacionalidad='Chilena'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$chile = mysqli_fetch_array($Chilena);
+//echo "chilenos :" . $chile['cantidad'];
+
+$Uruguaya = mysqli_query($conexion, "select count(*) as cantidad from usuarios where nacionalidad='Uruguay'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$uruguay = mysqli_fetch_array($Uruguaya);
+//echo "<br>urugayos :" . $uruguay['cantidad'];
+
+$Canadiense = mysqli_query($conexion, "select count(*) as cantidad from usuarios where nacionalidad='Canadiense'") or
+die("Problemas en el select:" . mysqli_error($conexion));
+$canada = mysqli_fetch_array($Canadiense);
+//echo "<br>canadienses :" . $canada['cantidad'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Inicio</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+  .usuario{
+  background-color: rgb(45, 123, 154);
+  height: 30px;
+  width: 100%;
+  text-align: center;
+}
+
+</style>
+<acript>
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          
+          ['Pais', 'Cantidad'],
+          ['Gente Chilena',     <?php echo $chile['cantidad']?>],
+          ['Gente Uruguaya',      <?php echo $uruguay['cantidad']?>],
+          ['Gente Canadiense',  <?php echo $canada['cantidad']?>],
+
+        ]);
+
+        var options = {
+          title: 'Nacionalidades'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
+</script>
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark"">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+              <a class="nav-link" aria-current="page" href="index.php">Inicio</a>
+              <?php
+              if($_SESSION["SessionState"]=="Active") 
+              {echo " 
+                <a class='nav-link' href='listausuarios.php'>Lista Personas</a> 
+                <a class='nav-link' href='lista_noticias.php'>Noticias</a>
+                <a class='nav-link active' href='estadisticas.php'>Estadisticas</a>";
+                if($_SESSION['SessionAdmin']==1){
+                  echo '<a class="nav-link" href="admin_registros.php">Administrar</a>
+                  <a class="nav-link" href="registrar_noticia">Ingresar Noticia</a>';
+                }
+              } else {
+                echo "<a class='nav-link' href='ingresar.php'>Ingresar</a>";
+                echo "<a class='nav-link' href='registro.php'>Registrarse</a>";
+              }
+              if($_SESSION["SessionState"]=="Active") 
+              {echo "<a class='nav-link' href='logout.php'>Salir</a>";}
+             ?>
+            </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="usuario">
+
+        <?php      
+      if (isset($_COOKIE["CORREO"])){
+        echo $_COOKIE["CORREO"];
+        echo $_COOKIE["ADMIN"];
+        }  
+      else { echo "Aun no a ingresado";}
+         ?>
+        </div>
+
+    </nav>
+
+
+    <div id="piechart" style="width: 1500px; height: 900px;"></div>
+
+    <br>
+
+    <footer class="text-center text-white fixed-bottom" style="background-color: #221144;">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          Tecnología Multimedia - CIF 6558 - 06/11/2022  <br>
+            Benjamin Gonzalez Fredes <br>
+            Nicolas Cepeda Zamorano
+
+        </div>
+      </footer>
+
+</body>
+
+</html>
