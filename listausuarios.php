80a3e436605767b1cc69763a3b5121e2f774e5c7
Multinecz <nicolas.cepeda@alumnos.upla.cl>
2022-12-22 01:49:01 -0300

    Usuarios comunes ya no pueden editar  los usuarios

--- listausuarios.php
+++ listausuarios.php
@@ -67,8 +67,13 @@ session_start();
         class='btn btn-danger btn-outline-secondary texto'>Perfil</button></a></td>
         <td><a href="crear_pdf.php?id=<?php echo $data["id"]?>&nombres=<?php echo $data["nombres"]?>&apellidos=<?php echo $data["apellidos"]?>&fecha_nacimiento=<?php echo $data["fecha_nacimiento"]?>&rut=<?php echo $data["rut"]?>&nacionalidad=<?php echo $data["nacionalidad"]?>&genero=<?php echo $data["genero"]?>&ciudad_residencia=<?php echo $data["ciudad_residencia"]?>&foto=<?php echo $data["foto"]?>">
         <button type='button' class='btn btn-info btn-outline-secondary'>Crear PDF</button></a></td>
-        <td><a href="editar.php?id=<?php echo $data["id"]?>"><button type='button' 
-        class='btn btn-outline-secondary'>Editar</button></a></td>
+        <?php if($_SESSION ['SessionAdmin']== 1 )
+         {
+          echo "<td><a href='editar.php?id=" . $data["id"]."'><button type='button' 
+           class='btn btn-outline-secondary'>Editar</button></a></td>";
+         }
+        
+        ?>
     </tr>
 <?php
           }
